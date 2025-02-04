e77106950e3fe87714f130328179beb108463bc7
AdriansTacosUW <adrianstacosuw@gmail.com>
2025-02-04 13:14:32 -0800

    Delete server directory

--- server/models/Review.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const reviewSchema = new mongoose.Schema({
-  text: String,
-  createdAt: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model('Review', reviewSchema);
